0273cde76fa3793dc39d84b956a2044665ac3f9c
Suprith39 <suprithns.25@gmail.com>
2024-06-02 07:02:58 +0530

    add java'

---
+++ tut3Static3.java
@@ -0,0 +1,16 @@
+class demo1{
+    static int x =10;
+    static void test(){
+        System.out.println("running the test() method");
+    }
+}
+public class tut3Static3 {
+    public static void main(String[] args) {
+        System.out.println("main method started");
+        System.out.println(" x value is :"+demo1.x);
+        demo1.test();
+        demo1.x=20;
+        System.out.println("x value is :"+demo1.x);
+        System.out.println("main method ended");
+    }
+}
